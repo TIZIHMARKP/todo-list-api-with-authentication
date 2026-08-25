d442b601919e75cd504aa9b957a7168e5d5ddf0f
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-25 05:58:36 -0700

    Imported the logger utils and added the before_request

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from config import Config
 from models import db, Todo
 from routes import todo_bp
 from services.todo_services import display_todos_api
+from utils.logger import log_request
 
 
 app = Flask(__name__)
@@ -11,6 +12,12 @@ app.config.from_object(Config)
 db.init_app(app)  # db init
 
 app.register_blueprint(todo_bp)   # blueprint reg
+
+@app.before_request
+def before_request():
+
+    log_request()  # all incoming request are visible in app.log
+
 
 with app.app_context():
     db.create_all()
