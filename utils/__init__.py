4a38263a07d48f50f9fd30ba3ec07e100eb5b464
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-25 05:57:24 -0700

    Added logging to log info into app.log

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 
 from .auth import auth, unauthorized
+from .logger import log_request, log_error, log_info
 
---
+++ utils/logger.py
@@ -0,0 +1,24 @@
+
+import logging
+from datetime import datetime
+from flask import request
+
+logging.basicConfig(
+    filename = 'app.log',
+    # level = logging.INFO,
+    level = logging.DEBUG,
+    format = '%(asctime)s - %(levelname)s - %(message)s',
+)
+
+def log_request():
+   
+    logging.info(f"Method: {request.method} PATH: {request.path}  IP: {request.remote_addr}")
+
+def log_error(message):
+
+    logging.error(f"Error: {message}")
+
+def log_info(message):
+    
+    logging.info(f"Info: {message}")
+
